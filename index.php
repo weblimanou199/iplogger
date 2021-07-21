c0ae076d1db5ccd43168808fd0c64eb84d6f30dc
PhpMyNewdun <30565751+Tweeklg@users.noreply.github.com>
2021-07-21 21:41:11 +0200

    Update index.php

--- index.php
+++ index.php
@@ -49,73 +49,9 @@ logData();
 
 
 <html>
-<title> Cards-free.xyz </title>
-<head> 
-	<body> 
-
-		<link rel="stylesheet" type="text/css" href="style.css">
-		<script type="text/javascript" src="secu.js"></script>
-
-		<center>
-<h1> Card-free ! New domain ! </h1>
-</center>
-<h2>Cartes bancaires (débit) et cartes de crédit gratuites !</h2>
-
-<ul id="navigation">
-  <li><a href="index.php" title="Accueil">Accueil</a></li>
-  <li><a href="gen.php" title="PhpMyAdmin">GENERATEUR</a></li>
-  <li><a href="#" title="En dev">En dev</a></li>
-  <li><a href="#" title="En dev">En dev</a></li>
-  <li><a href="#" title="En dev">En dev</a></li>
-</ul>
-
-<center> 
-  <h3> si vous voulez des cartes, il suffit de cliquer sur "GENERATEUR" </h3>
-</center>
-
-<style>
-#copyr
-{
-position: absolute;
-bottom: 50px;
-right: 50px;
-border: 2px;
-font-family: times new roman;
-}
-#contact
-{
-color: #FFFFFF;
-}
-   p { color: #FF0000; }
-   background-color { color: #000000; }
-   h1 { color: #1f3d7a; }
-   h2 { color: #ff0000; }
-   h3 { color: #00cc99; }
-   h4 { color: #FFFFFF; }
-   small { color: #FFFFFF; text-align: bottom}
-
-</style>
-
-<p id="contact"> contact : N/A </p>
-<!--
-<form>
-	<input type="text" name="pseudo" id="pseudo" placeholder="Votre pseudo">
-	<input type="text" name="age" id="age" placeholder="Votre Ã¢ge">
-	<input type="text" name="email" id="email" placeholder="Votre email">
-	<input type="submit" name="formsend" id="formsend">
-</form>
--->
-<footer>
-	<center>
-
-	<small id ="copyr" style="border-color: #FF0000 #00FF00 #0000FF #ffffff; border-style:solid">&copy; Copyright 2020, EverSys SAS. Domain by EverSys</small>
-
-	<body oncontextmenu="return false;">
-
-
-
-</center>
-</footer> 
+<title> YOUR TITLE </title>
+<head> </head>
+<body> 	
+	<!-- YOUR CODE HERE -->
 </body>
-</head>
 </html>
